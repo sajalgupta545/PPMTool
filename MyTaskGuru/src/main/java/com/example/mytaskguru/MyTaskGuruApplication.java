8496840f32b85bc9601dd1a546943c4636104d9d
prateek1o1 <prateekchhimwal@gmail.com>
2023-04-14 16:51:47 +0530

    First Commit

---
+++ MyTaskGuru/src/main/java/com/example/mytaskguru/MyTaskGuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytaskguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaskGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTaskGuruApplication.class, args);
+	}
+
+}
---
+++ MyTaskGuru/src/test/java/com/example/mytaskguru/MyTaskGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytaskguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTaskGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
