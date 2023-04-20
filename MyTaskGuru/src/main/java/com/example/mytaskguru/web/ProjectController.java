6199350aa7220c6eafce21dc7ffbde31f103eea6
prateek1o1 <prateekchhimwal@gmail.com>
2023-04-20 13:17:35 +0530

    Reducer

---
+++ MyTaskGuru/src/main/java/com/example/mytaskguru/web/ProjectController.java
@@ -0,0 +1,53 @@
+package com.example.mytaskguru.web;
+
+import com.example.mytaskguru.domain.Project;
+import com.example.mytaskguru.services.MapValidationErrorService;
+import com.example.mytaskguru.services.ProjectService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap =mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null)
+            return errorMap;
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+
+        Project project = projectService.findProjectByIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects()
+    {
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted", HttpStatus.OK);
+    }
+}
