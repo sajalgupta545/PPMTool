128d014ea3ae3f7260f62146b84ebb4c5d6dbbbf
prateek1o1 <prateekchhimwal@gmail.com>
2023-04-23 01:29:09 +0530

    Added Domain and Repositories

--- MyTaskGuru/src/main/java/com/example/mytaskguru/domain/Project.java
+++ MyTaskGuru/src/main/java/com/example/mytaskguru/domain/Project.java
@@ -1,6 +1,7 @@
 package com.example.mytaskguru.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,28 +11,40 @@ import java.util.*;
 @Entity
 public class Project {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        @NotBlank(message="Project name is required")
-        private String projectName;
-        @NotBlank(message="Project identifier is required")
-        @Size(min=4,max=5,message="Please use 4 to 5 characters")
-        @Column(updatable = false,unique = true)
-        private String projectIdentifier;
-        @NotBlank(message = "Project description is required")
-        private String description;
-        @JsonFormat(pattern="yyyy-mm-dd")
-        private Date start_date;
-        @JsonFormat(pattern="yyyy-mm-dd")
-        private Date end_date;
-        @JsonFormat(pattern="yyyy-mm-dd")
-        private Date created_At;
-        @JsonFormat(pattern="yyyy-mm-dd")
-        private Date updated_At;
-        @OneToOne(fetch=FetchType.EAGER,cascade = CascadeType.ALL,mappedBy = "project")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+    @NotBlank(message ="Project Identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
+    private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
+    private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    @Column(updatable = false)
+    private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_At;
 
-        private Backlog backlog;
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+    @JsonIgnore
+    private Backlog backlog;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+
+    private String projectLeader;
+
+
+
     public Project() {
     }
 
@@ -107,14 +120,30 @@ public class Project {
         this.backlog = backlog;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getProjectLeader() {
+        return projectLeader;
+    }
+
+    public void setProjectLeader(String projectLeader) {
+        this.projectLeader = projectLeader;
+    }
+
     @PrePersist
-        protected void onCreate()
-        {
-            this.created_At=new Date();
-        }
-        @PreUpdate
-        protected void onUpdate()
-        {
-            this.updated_At=new Date();
-        }
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
 }
