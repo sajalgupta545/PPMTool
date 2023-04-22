247d5f1317e829a0735e3795815a99ae8e47faa1
prateek1o1 <prateekchhimwal@gmail.com>
2023-04-23 00:46:00 +0530

    Added Backlog & ProjectTask

---
+++ MyTaskGuru/src/main/java/com/example/mytaskguru/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.mytaskguru.repositories;
+
+import com.example.mytaskguru.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
