ce6c5594aec1251fff05c98ff5a181a5bce6acb1
prateek1o1 <prateekchhimwal@gmail.com>
2023-05-12 19:33:18 +0530

    UserServiceTest

--- MyTaskGuru/src/main/java/com/example/mytaskguru/services/UserService.java
+++ MyTaskGuru/src/main/java/com/example/mytaskguru/services/UserService.java
@@ -5,10 +5,12 @@ import com.example.mytaskguru.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @Service
 public class UserService {
 
+    private static final Logger logger= LogManager.getLogger(UserService.class);
     @Autowired
     private UserRepository userRepository;
 
@@ -19,13 +21,16 @@ public class UserService {
     public User saveUser (User newUser){
 
         try{
+            logger.info("[saveUser] - "+newUser.toString());
             newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
             //Username has to be unique (exception)
             newUser.setUsername(newUser.getUsername());
             // Make sure that password and confirmPassword match
             // We don't persist or show the confirmPassword
             newUser.setConfirmPassword("");
-            return userRepository.save(newUser);
+            User u=userRepository.save(newUser);
+            logger.info("[RESULT - saveUser] - "+u.toString());
+            return u;
 
         }catch (Exception e){
             throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
